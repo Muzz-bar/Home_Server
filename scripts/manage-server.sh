5c5ac0c645fb5575fd0fd64d70cf1e66e451152c
Muzz-bar <amujadidr@gmail.com>
2026-06-18 16:27:35 +0700

    typo jir

--- scripts/manage-server.sh
+++ scripts/manage-server.sh
@@ -5,21 +5,24 @@ HIJAU='\033[0;32m'
 MERAH='\033[0;31m'
 NORMAL='\033[0m'
 
-# Kumpulan folder cluster CORE (Wajib nyala duluan)
+# Kunci folder induk server secara absolut menggunakan direktori home lu
+BASE_DIR="/home/muzz/my_serverku"
+
+# Kumpulan folder cluster CORE (Wajib dikasih prefix $BASE_DIR)
 CORE_SERVICES=(
-    "docker/core/tailscale"
-    "docker/core/cloudflare"
-    "docker/core/portainer"
+    "$BASE_DIR/docker/core/tailscale"
+    "$BASE_DIR/docker/core/cloudflare"
+    "$BASE_DIR/docker/core/portainer"
 )
 
-# Kumpulan folder cluster APPS
+# Kumpulan folder cluster APPS (Wajib dikasih prefix $BASE_DIR)
 APP_SERVICES=(
-    "docker/apps/file_browser"
-    "docker/apps/linkstack"
-    "docker/apps/nextcloud"
-    "docker/apps/stirling"
-    "docker/apps/tabby-web"
-    "docker/apps/uptime kuma"
+    "$BASE_DIR/docker/apps/file_browser"
+    "$BASE_DIR/docker/apps/linkstack"
+    "$BASE_DIR/docker/apps/nextcloud"
+    "$BASE_DIR/docker/apps/stirling"
+    "$BASE_DIR/docker/apps/tabby-web"
+    "$BASE_DIR/docker/apps/uptime kuma"
 )
 
 case "$1" in
